e0fcc8e73ae65c9fd62a8a42ca388610a7c03f4d
BantitaC <bantita.chk@gmail.com>
2025-05-25 22:45:20 +0700

    install

---
+++ frontend/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Button, dividerClasses } from '@mui/material'
+
+export default function Home() {
+  return (
+    <div>
+
+    </div>
+  )
+}
